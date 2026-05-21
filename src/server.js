e220b7523efa241b912e419e5ceb8b15ca54569c
Linda amaya <linda.amaya.cetina@gmail.com>
2026-05-21 00:02:48 -0500

    pruebo 8

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 const pLimit = require("p-limit").default;
 
 const app = express();
@@ -8,28 +9,21 @@ const app = express();
 // CORS GLOBAL FIX
 // ===============================
 
-app.use((req, res, next) => {
+const cors = require("cors");
 
-  res.header("Access-Control-Allow-Origin", "*");
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: [
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "Authorization"
+  ]
+}));
 
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-
-  // PREFLIGHT
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-
-});
+app.options("*", cors());
 
 app.use(express.json({
   limit: "50mb"
