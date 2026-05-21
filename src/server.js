987b25903ab1558476065d3671985c9950542659
Linda amaya <linda.amaya.cetina@gmail.com>
2026-05-20 23:15:28 -0500

    ajsute cors

--- src/server.js
+++ src/server.js
@@ -5,7 +5,13 @@ const pLimit = require("p-limit").default;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+app.options(/.*/, cors());
 
 app.use(express.json({
   limit: "50mb"
