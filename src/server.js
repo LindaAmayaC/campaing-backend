d265574581ea5c918a89a80b3293bf84f97a5018
Linda amaya <linda.amaya.cetina@gmail.com>
2026-05-21 00:38:14 -0500

    prueba17

--- src/server.js
+++ src/server.js
@@ -9,11 +9,14 @@ const app = express();
 app.use(cors());
 
 app.use((req, res, next) => {
+
   res.header("Access-Control-Allow-Origin", "*");
+
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+
   res.header(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, OPTIONS"
@@ -24,7 +27,10 @@ app.use((req, res, next) => {
   }
 
   next();
+
 });
+
+app.options(/.*/, cors());
 
 app.use(express.json({
   limit: "50mb"
