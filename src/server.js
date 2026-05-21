17cf61f315b9179a1505b8493403b0e003c872d3
Linda amaya <linda.amaya.cetina@gmail.com>
2026-05-21 01:37:19 -0500

    prueba 20

--- src/server.js
+++ src/server.js
@@ -6,31 +6,17 @@ const pLimit = require("p-limit").default;
 
 const app = express();
 
-app.use(cors());
-
-app.use((req, res, next) => {
-
-  res.header("Access-Control-Allow-Origin", "*");
-
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS"
-  );
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-
-});
-
-
+app.use(cors({
+  origin: true,
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: [
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "Authorization"
+  ],
+}));
 app.use(express.json({
   limit: "50mb"
 }));
