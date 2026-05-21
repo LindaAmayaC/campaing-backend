ce204d20ee8f1da26eb1c5655772e21456da8c02
Linda amaya <linda.amaya.cetina@gmail.com>
2026-05-21 00:57:23 -0500

    cambio 12

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 
   res.header(
     "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
+    "GET, POST, OPTIONS"
   );
 
   if (req.method === "OPTIONS") {
@@ -30,7 +30,6 @@ app.use((req, res, next) => {
 
 });
 
-app.options(/.*/, cors());
 
 app.use(express.json({
   limit: "50mb"
